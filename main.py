a00193a7cd65b10123116419e7b7ace61728cdc2
Mateus Santos <mateussantos.leega@sulamerica.com.br>
2025-01-21 23:53:32 -0300

    run etl and setup in main file

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
-# from datetime import datetime, timedelta
-#
-# # Main ETL process
-# def etl_process():
-#     jwt_token = generate_jwt()
-#
-#     start_date = datetime(2024, 12, 1)
-#     end_date = datetime(2024, 12, 31)
-#
-#     current_date = start_date
-#     while current_date <= end_date:
-#         day_str = current_date
-#         data = fetch_sales_data(jwt_token, day_str, day_str)
-#         load_data_to_snowflake(data, temp_table)
-#         current_date += timedelta(days=1)
-#
-#     merge_temp_to_prod()
 from dotenv import load_dotenv
 load_dotenv()
 
 if __name__ == "__main__":
-    from src.config.setup import test_credentials
-    test_credentials()
+    from datetime import datetime, timedelta
+    from src.pipeline.etl import SalesReportELT
+    from src.sinks import SnowFlake
+    from src.sources import AppStoreAPI
+    import setup
+    setup.run(force_create_tables=True)
+    print("Setup completed")
+
+    elt = SalesReportELT(sink=SnowFlake(), source=AppStoreAPI())
+
+    start_date = datetime(2024, 12, 1)
+    end_date = datetime(2024, 12, 2)
+    current_date = start_date
+    while current_date <= end_date:
+        print(f"Processing {current_date}")
+        elt.run(current_date)
+        current_date += timedelta(days=1)
