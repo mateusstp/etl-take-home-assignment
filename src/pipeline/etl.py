fd1e9fb323ccc39281f62d7a83baa2fa4cbd1f9c
Mateus Santos <mateussantos.leega@sulamerica.com.br>
2025-01-21 23:53:00 -0300

    add elt extract and load temp data

--- src/pipeline/etl.py
+++ src/pipeline/etl.py
@@ -1,37 +1,41 @@
 from abc import ABC, abstractmethod
-class  ETL(ABC):
+from src.sources.app_store_api import AppStoreAPI
+from src.sinks.snowflake_sink import SnowFlake
+
+
+class ELT(ABC):
 
     @abstractmethod
-    def extract(self):
+    def extract(self, *args, **kwargs):
         pass
 
     @abstractmethod
-    def transform(self):
+    def transform(self, *args, **kwargs):
         pass
 
     @abstractmethod
-    def load(self):
+    def load(self, *args, **kwargs):
         pass
 
     @abstractmethod
-    def run(self):
+    def run(self, *args, **kwargs):
         pass
-class SalesReportETL(ETL):
-    def __init__(self, source, sink):
+
+
+class SalesReportELT(ELT):
+    def __init__(self, source: AppStoreAPI, sink: SnowFlake):
         self.source = source
         self.sink = sink
 
-    def extract(self):
-       pass
+    def extract(self, report_date):
+        return self.source.get_sales_by_day(report_date)
+
+    def load(self, csv_file_path):
+        self.sink.load(csv_file_path, self.sink.temp_table)
 
     def transform(self):
-     pass
+        pass
 
-    def load(self):
-      pass
-
-    def run(self):
-        self.extract()
-        self.transform()
-        self.load()
-
+    def run(self, report_date):
+        csv_file_path = self.extract(report_date)
+        self.load(csv_file_path)
