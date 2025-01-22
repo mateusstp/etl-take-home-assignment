e7345b9d8298b6c63ee2fd346c8480d21dbe0ec5
Mateus Santos <mateussantos.leega@sulamerica.com.br>
2025-01-22 09:12:46 -0300

    test snowflake duplicated data

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ if __name__ == "__main__":
     from src.sinks import SnowFlake
     from src.sources import AppStoreAPI
     import setup
-    setup.run(force_create_tables=True)
+    setup.run(force_create_tables=False)
     print("Setup completed")
 
     elt = SalesReportELT(sink=SnowFlake(), source=AppStoreAPI())
 
     start_date = datetime(2024, 12, 1)
-    end_date = datetime(2024, 12, 2)
+    end_date = datetime(2024, 12, 10)
     current_date = start_date
     while current_date <= end_date:
         print(f"Processing {current_date}")
